7d57564c37a38198e1e0ee0b90b279e65170c337
Raquel A Fonseca <raquel.fonseca@ccc.ufcg.edu.br>
2020-10-18 16:59:20 -0300

    graficos com dados manunais

--- client/src/components/BarChart/index.js
+++ client/src/components/BarChart/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { Container } from './style';
+import { Chart} from "react-google-charts";
+
 
 const BarChart = () => {
+    const [options, setOptions] = React.useState({
+        title: 'Sites mais acessados'
+      })
+      const [data, setDataBar] = React.useState([
+        ['Sites', 'Acessos'],
+        ['Google', 100],
+        ['Youtube', 80],
+        ['Facebook', 50],
+      ])
 
     return (
         <Container>
-            BARRAS 
+            <Chart
+            width={'500px'}
+            height={'300px'}
+            chartType="Bar"
+            data={data}
+            options={options}
+        />
         </Container>
     )
 }
--- client/src/components/PieChart/index.js
+++ client/src/components/PieChart/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { Container } from './style';
+import { Chart} from "react-google-charts";
+
 
 const PieChart = () => {
-
+    const [options, setOptions] = React.useState({
+        title: 'Sites mais acessados'
+      })
+      const [data, setData] = React.useState([
+        ['Linguagens', 'Quantidade'],
+        ['Google', 100],
+        ['Youtube', 80],
+        ['Facebook', 50],
+      ])
+    
     return (
-        <Container>
-            PIZZA 
-        </Container>
-    )
+    <Container>
+        <Chart
+            width={'500px'}
+            height={'300px'}
+            chartType="PieChart"
+            data={data}
+            options={options}
+        />
+    </Container>   
+      );
 }
 
 export default PieChart;
