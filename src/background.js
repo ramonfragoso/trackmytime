27511860f111d0e412c2ef06739a2438c9e1d316
Hugo Galvão <hugo.galvao@ccc.ufcg.edu.br>
2020-11-04 08:47:34 -0300

    Fix installation errors

--- src/background.js
+++ src/background.js
@@ -14,7 +14,8 @@ chrome.runtime.onInstalled.addListener(function(details){
     chrome.storage.local.set({
         shouldMonitor: true,
         data: {
-        } 
+        },
+        last_visited: "" 
     })
     /*
     'shouldMonitor': false,
