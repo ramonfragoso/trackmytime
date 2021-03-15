5db280b29b1b1f2c2d4c086e4a9a54b0e2a07ff3
ramonfragoso <ramon.silva@bitpreco.com>
2021-03-15 04:32:43 -0300

    Adding buttons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import { Container, Icon, Title, Left, Right, StyledLink } from './style';
+import React, { useState } from 'react';
+import { Container, Icon, Title, Label, Left, Right, StyledLink, BulbIcon } from './style';
+import Switch from "react-switch";
 import image from './icon.png';
+import on from './bulbOn.png';
+import off from './bulbOff.png';
 
 const Header = () => {
+
+    const [darkMode, setDarkMode] = useState(true)
+    const [isCounterOn, setIsCounterOn] = useState(true)
+
+    const handleDarkMode = () => {
+        setDarkMode(!darkMode);
+        //TODO : MUDAR DE FATO O MODO NEGRO
+    }   
+
+    const handleCounter = () => {
+        setIsCounterOn(!isCounterOn);
+        //TODO : LIGAR OU DESLIGAR O CONTADOR
+    }  
 
     return (
         <Container>
@@ -11,8 +27,19 @@ const Header = () => {
                 <Title>TrackMyTime</Title>
             </Left>
             <Right>
-                <StyledLink>Contato</StyledLink>
-                <StyledLink>Equipe</StyledLink>
+                <StyledLink>
+                    <Label>ON/OFF</Label>
+                    <Switch 
+                        height={20} 
+                        width={45} 
+                        checkedHandleIcon={null} 
+                        uncheckedHandleIcon={null} 
+                        checkedIcon={null} 
+                        uncheckedIcon={null} 
+                        onChange={handleCounter} checked={isCounterOn} 
+                    />
+                </StyledLink>
+                {darkMode ? <BulbIcon src={on} onClick={handleDarkMode}/> : <BulbIcon src={off} onClick={handleDarkMode}/> }    
             </Right>
         </Container>
     )
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,17 +1,26 @@
 import styled from 'styled-components';
-import theme from '../../views/Popup/theme';
 
 export const Container = styled.div`
     width: 100%;
     display: flex;
     align-items: center;    
     justify-content:space-between;
+    padding-top: 20px;
 `;
 
 export const Icon = styled.img`
-    width: 78px;
-    height: 78px;
+    width: 65px;
+    height: 65px;
     margin-right: 20px;
+`;
+
+export const BulbIcon = styled.img`
+    width: 50px;
+    height: 50px;
+`;
+
+export const Label = styled.div`
+    margin: 10px 0px;
 `;
 
 export const Title = styled.div`
@@ -36,13 +45,15 @@ export const StyledLink = styled.div`
     user-select: none;
     display: flex;
     align-items: center;
+    justify-content: center;
+    flex-direction: column;
     font-weight: 400;
     font-size: 120%;
     margin: 0px 20px;
     transition: ease-in-out .1s;
     height: 100%;
+    color: white;
     &:hover {
         cursor: pointer;
-        color: white;
     }
 `; 
